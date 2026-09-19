2a883fecb50b92b293b20ebfcdac9b707838ee19
Florent Girault Nicolas <fgirault@c1r4p2.42nice.fr>
2026-09-19 16:29:01 +0200

    rush01

--- c/c04/ex02/ft_putnbr.c
+++ c/c04/ex02/ft_putnbr.c
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgirault <fgirault@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fgirault <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/17 15:30:26 by fgirault          #+#    #+#             */
-/*   Updated: 2026/09/18 12:49:14 by fgirault         ###   ########.fr       */
+/*   Created: 2026/09/16 13:56:15 by fgirault          #+#    #+#             */
+/*   Updated: 2026/09/17 14:16:17 by fgirault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void	ft_putnbr(int nb);
-void	ft_putchar(char c);
+int	ft_int_len(int nb);
+char	reverse_str(char *str, int nb);
 
 int	main(void)
 {
@@ -23,26 +24,48 @@ int	main(void)
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
+	int	const_nb;
+	char	char_nb[ft_int_len(nb)];
+	int	i;
+
+	const_nb = nb;
+	i = 0;
+	while (nb != 0)
 	{
-		if (nb == -2147483648)
-		{
-			write(1, "-2147483648", 11);
-			return;
-		}
-		ft_putchar('-');
-		nb *= -1;
+		char_nb[i] = nb % 10 + 48;
+		nb  = (nb - (nb % 10)) / 10;
+		i++;
 	}
-	if ((nb / 10) == 0)
-		ft_putchar('0' + nb);
-	else
+	while (reverse_str(char_nb, const_nb) != '\0')
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		write(1, reverse_str(char_nb, const_nb), 1);
+		reverse_str++;
 	}
 }
 
-void	ft_putchar(char c)
+int	ft_int_len(int nb)
 {
-	write(1, &c, 1);
+	int	int_len;
+
+	int_len = 0;
+	while(nb != 0)
+	{
+		nb  = (nb - (nb % 10)) / 10;
+		int_len++;
+	}
+	return(int_len);
+}
+
+char	reverse_str(char *str, int nb)
+{
+	int	i;
+	char	output_str[ft_int_len(nb)];
+
+	i = 0;
+	while (i <= ft_int_len(nb))
+	{
+		output_str[i] = str[ft_int_len(nb) - i - 1];
+		i++;
+	}
+	return (&output_str);
 }
